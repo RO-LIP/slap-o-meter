d22b6979103922b276064b38cebe68fdd1bf676a
Sydrimon <sydrimon@gmail.com>
2020-03-05 14:41:13 +0100

    slap-o-meter v1.0

--- __init__.py
+++ __init__.py
@@ -1,39 +1,42 @@
 import bhi160
+import buttons
 import display
 import leds
 import utime
 
-bhi = bhi160.BHI160Accelerometer()
+# init
+bhi = bhi160.BHI160Accelerometer(sample_rate=4)
 disp = display.open()
-
+leds.clear()
 highest = 0
+
 while True:
+	# reset if button pressed
+	pressed = buttons.read(buttons.BOTTOM_LEFT | buttons.BOTTOM_RIGHT)
+	if pressed & buttons.BOTTOM_LEFT != 0: highest = 0
+
+	# read accel
 	samples = bhi.read()
 	if len(samples) == 0:
-		utime.sleep(0.5)
+		utime.sleep(0.25)
 		continue
-	# print the latest sample
-	disp.clear()
-	#disp.print(str(samples[-1].x), posy=0, fg=[255, 255, 255])
-	#disp.print(str(samples[-1].y), posy=20, fg=[255, 255, 255])
-	#disp.print(str(samples[-1].z), posy=40, fg=[255, 255, 255])
+
 	for val in samples:
-		complete = abs(samples[-1].x + samples[-1].y + samples[-1].z)
+		complete = abs(val.x) + abs(val.y) + abs(val.z) - 1
 		if complete > highest: highest = complete
-	disp.print(str(highest), posy=40, fg=[255, 255, 255])
+
+	# update display
+	disp.clear()
+	disp.print("Slap force:", posy=0, fg=[255, 255, 255])
+	disp.print("<- reset", posy=60, fg=[255, 0, 0])
+	disp.print(str(highest), posy=20, posx=20, fg=[255, 255, 255])
 	disp.update()
 
+	# Update LEDs
 	color = [0, 255, 0] if highest < 3 else [255, 0, 0]
-	if highest > 1:
-		leds.set(10, color)
-	if highest > 1.5:
-		leds.set(9, color)
-	if highest > 2:
-		leds.set(8, color)
-	if highest > 2.5:
-		leds.set(7, color)
-	if highest > 3:
-		leds.set(6, color)
+	for i in range(0, 11):
+		if highest*2 < i: color = [0, 0, 0] 
+		leds.prep(10 - i, color)
+	leds.update()
 
-	utime.sleep(1)
-	leds.clear()
+	utime.sleep(0.25)
